63b13a229109f1f8dc970d44077015cd68be6788
vamsi <vamsikrishna.kuppa@gmail.com>
2017-09-20 21:55:33 +0530

    Changed top nav with the login and logout

--- LightsOutTool.py
+++ LightsOutTool.py
@@ -7,3 +7,30 @@ from flask_wtf.csrf import CSRFProtect
 csrf = CSRFProtect(app)
 app.secret_key = 'oracle'
 app.run(debug=True)
+
+#Configurations
+# class BaseConfig(object):
+#     """Base config class"""
+#     SECRET_KEY = 'oracle'
+#     DEBUG = True
+#     TESTING = False
+#     NEW_CONFIG_VARIABLE = 'my value'
+#
+#
+# class ProductionConfig(BaseConfig):
+#     """Production specific config"""
+#     DEBUG = False
+#     # SECRET_KEY = open('/path/to/secret/file').read()
+#
+#
+# class StagingConfig(BaseConfig):
+#     """Staging specific config"""
+#     DEBUG = True
+#
+#
+# class DevelopmentConfig(BaseConfig):
+#     """Development environment specific config"""
+#     DEBUG = True
+#     TESTING = True
+#     # SECRET_KEY = 'Another random secret key'
+
