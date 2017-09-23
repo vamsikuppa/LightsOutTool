ddfa04962c094ee2d03a109c58c762122e9910d8
vamsi <vamsikrishna.kuppa@gmail.com>
2017-09-23 17:48:06 +0530

    updated config to init.py and removed local dev server

--- my_app/__init__.py
+++ my_app/__init__.py
@@ -8,6 +8,7 @@ from flask_login import LoginManager
 app = Flask(__name__)
 
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@localhost:3306/lightsouttool'
+app.config.from_object('config.ProductionConfig')
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 manager = Manager(app)
@@ -19,7 +20,6 @@ login_manager.login_view = 'login'
 
 
 from my_app.product.views import app
-
 
 
 db.create_all()
