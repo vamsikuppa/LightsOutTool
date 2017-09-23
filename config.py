db085d3449247e68033f00a3df57d82c77e9bad7
vamsi <vamsikrishna.kuppa@gmail.com>
2017-09-23 09:09:50 +0530

    added staging config for config.py

--- config.py
+++ config.py
@@ -17,11 +17,11 @@ class ProductionConfig(BaseConfig):
 class StagingConfig(BaseConfig):
     """Staging specific config"""
     DEBUG = True
-
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:root@localhost:3306/lightsouttool'
 
 class DevelopmentConfig(BaseConfig):
     """Development environment specific config"""
     DEBUG = True
     TESTING = True
     # SECRET_KEY = 'Another random secret key'
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:root@localhost:3306/lightsouttool'
+    #SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:root@localhost:3306/lightsouttool'
